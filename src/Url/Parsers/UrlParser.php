3faac77c7cae243f2d20b449a6460f59ebb87506
Andrii Indenko <a.indenko@tech.leaseweb.com>
2015-08-11 11:44:58 +0200

    feature/getProtocol: Created getProtocol

--- src/Url/Parsers/UrlParser.php
+++ src/Url/Parsers/UrlParser.php
@@ -2,6 +2,10 @@
 
 namespace Url\Parsers;
 
+/**
+ * Class UrlParser
+ * @package Url\Parsers
+ */
 class UrlParser {
 
     private $url;
@@ -10,12 +14,45 @@ class UrlParser {
     private $username;
     private $query;
 
+    private $protocolSchemas = array(
+        'http' => 'HTTP',
+        'https'=> 'HTTPS'
+    );
+
+    /**
+     * @param $url
+     * @throws \Exception
+     */
     function __construct($url){
+        if(!empty($url)) {
+            $this->url = $url;
+        } else {
+            throw new \Exception("Url has not to be empty");
+        }
+        //TODO remove ./  ../
+
+        $this->parse($this->url);
 
     }
 
-    function parse(){
+    /**
+     * Parse Url
+     *
+     * @throws \Exception
+     */
+    private function parse(){
+        $res = parse_url($this->url);
+        if(isset($res['scheme'])){
+            if(in_array($res['scheme'],array_keys($this->protocolSchemas))){
+                $this->protocol = $this->protocolSchemas[$res['scheme']];
+            } else {
+                throw new \Exception("Given protocol not detected");
+            }
+        }
+    }
 
+    public function getProtocol(){
+        return $this->protocol;
     }
 
 }
--- src/Url/Tests/UrlParserTest.php
+++ src/Url/Tests/UrlParserTest.php
@@ -5,11 +5,14 @@ namespace Url\Tests;
 use Url\Parsers\UrlParser;
 
 class UrlParserTest extends \PHPUnit_Framework_TestCase{
- 
+
+    public $correctUrl = "http://andrey@example.com:80/test?query";
+
     public function testGetProtocol(){
 
-        $urlParser = new UrlParser();
-        $this->assertTrue(true,true);
+        $urlParser = new UrlParser($this->correctUrl);
+        $protocol = $urlParser->getProtocol();
+        $this->assertEquals($protocol,'HTTP');
 
    }
 
