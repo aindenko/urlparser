fc47870a4e7cb0cfac78ea26feb09157b6b2835f
Andrii Indenko <a.indenko@tech.leaseweb.com>
2015-08-11 12:39:57 +0200

    feature/toString: php-cs-fixer run

--- src/Url/Parsers/UrlParser.php
+++ src/Url/Parsers/UrlParser.php
@@ -3,11 +3,10 @@
 namespace Url\Parsers;
 
 /**
- * Class UrlParser
- * @package Url\Parsers
+ * Class UrlParser.
  */
-class UrlParser {
-
+class UrlParser
+{
     private $url;
     private $protocol = '';
     private $port = '';
@@ -18,165 +17,164 @@ class UrlParser {
 
     private $protocolSchemas = array(
         'http' => 'HTTP',
-        'https'=> 'HTTPS'
+        'https' => 'HTTPS',
     );
 
     /**
      * @param $url
+     *
      * @throws \Exception
      */
-    function __construct($url){
-        if(!empty($url)) {
+    public function __construct($url)
+    {
+        if (!empty($url)) {
             $this->url = $url;
         } else {
-            throw new \Exception("Url has not to be empty");
+            throw new \Exception('Url has not to be empty');
         }
         //TODO remove ./  ../
 
         $this->parse($this->url);
-
     }
 
     /**
-     * Parse Url
+     * Parse Url.
      *
      * @throws \Exception
      */
-    private function parse(){
+    private function parse()
+    {
         $res = parse_url($this->url);
 
         //parse protocol
-        if(isset($res['scheme'])){
-            if(in_array($res['scheme'],array_keys($this->protocolSchemas))){
+        if (isset($res['scheme'])) {
+            if (in_array($res['scheme'], array_keys($this->protocolSchemas))) {
                 $this->protocol = $this->protocolSchemas[$res['scheme']];
             } else {
-                throw new \Exception("Given protocol not detected");
+                throw new \Exception('Given protocol not detected');
             }
         }
         //parse user
-        if(isset($res['user'])){
+        if (isset($res['user'])) {
             $this->user = $res['user'];
         }
 
         //parse host
-        if(isset($res['host'])){
+        if (isset($res['host'])) {
             $this->host = $res['host'];
         }
 
         //parse port
-        if(isset($res['port'])){
+        if (isset($res['port'])) {
             $this->port = $res['port'];
         }
 
         //parse query
-        if(isset($res['query'])){
+        if (isset($res['query'])) {
             $this->query = $res['query'];
         }
 
         //parse path
-        if(isset($res['path'])){
+        if (isset($res['path'])) {
             $this->path = $res['path'];
         }
-
-
     }
 
     /**
-     * Getter for protocol
+     * Getter for protocol.
      *
      * @return mixed
      */
-    public function getProtocol(){
+    public function getProtocol()
+    {
         return $this->protocol;
     }
 
     /**
-     * Getter for user
+     * Getter for user.
      *
      * @return mixed
      */
-    public function getUser(){
+    public function getUser()
+    {
         return $this->user;
     }
 
-
     /**
-     * Getter for port
+     * Getter for port.
      *
      * @return mixed
      */
-    public function getPort(){
+    public function getPort()
+    {
         return $this->port;
     }
 
-
     /**
-     * Getter for query
+     * Getter for query.
      *
      * @return mixed
      */
-    public function getQuery(){
+    public function getQuery()
+    {
         return $this->query;
     }
 
     /**
-     * Getter for host
+     * Getter for host.
      *
      * @return mixed
      */
-    public function getHost(){
+    public function getHost()
+    {
         return $this->host;
     }
 
-
     /**
-     * Getter for path
+     * Getter for path.
      *
      * @return mixed
      */
-    public function getPath(){
+    public function getPath()
+    {
         return $this->path;
     }
 
     /**
-     * Magic happens here :)
+     * Magic happens here :).
      *
      * @return string
      */
-    public function __toString(){
-
+    public function __toString()
+    {
         $url = '';
 
         $schemas = array_flip($this->protocolSchemas);
 
-        if($schemas[$this->protocol]){
-            $url .= $schemas[$this->protocol] . '://';
+        if ($schemas[$this->protocol]) {
+            $url .= $schemas[$this->protocol].'://';
         }
 
-        if($this->user){
-            $url .=  $this->user . '@';
+        if ($this->user) {
+            $url .=  $this->user.'@';
         }
 
-        if($this->host){
-            $url .=  $this->host ;
+        if ($this->host) {
+            $url .=  $this->host;
         }
 
-        if($this->port){
-            $url .=  ':' . $this->port ;
+        if ($this->port) {
+            $url .=  ':'.$this->port;
         }
 
-        if($this->path){
-            $url .=  $this->path ;
+        if ($this->path) {
+            $url .=  $this->path;
         }
 
-        if($this->query){
-            $url .= '?' . $this->query;
+        if ($this->query) {
+            $url .= '?'.$this->query;
         }
 
         return  $url;
     }
-
 }
-
-
-
--- src/Url/Tests/UrlParserTest.php
+++ src/Url/Tests/UrlParserTest.php
@@ -4,62 +4,56 @@ namespace Url\Tests;
 
 use Url\Parsers\UrlParser;
 
-class UrlParserTest extends \PHPUnit_Framework_TestCase{
+class UrlParserTest extends \PHPUnit_Framework_TestCase
+{
+    public $correctUrl = 'http://andrey@example.com:80/test?query';
 
-    public $correctUrl = "http://andrey@example.com:80/test?query";
-
-    public function testGetProtocol(){
-
+    public function testGetProtocol()
+    {
         $urlParser = new UrlParser($this->correctUrl);
         $protocol = $urlParser->getProtocol();
-        $this->assertEquals($protocol,'HTTP');
+        $this->assertEquals($protocol, 'HTTP');
+    }
 
-   }
-
-    public function testGetUser(){
-
+    public function testGetUser()
+    {
         $urlParser = new UrlParser($this->correctUrl);
         $user = $urlParser->getUser();
-        $this->assertEquals($user,'andrey');
-
+        $this->assertEquals($user, 'andrey');
     }
 
-    public function testGetQuery(){
-
+    public function testGetQuery()
+    {
         $urlParser = new UrlParser($this->correctUrl);
         $query = $urlParser->getQuery();
-        $this->assertEquals($query,'query');
-
+        $this->assertEquals($query, 'query');
     }
 
-    public function testGetHost(){
-
+    public function testGetHost()
+    {
         $urlParser = new UrlParser($this->correctUrl);
         $host = $urlParser->getHost();
-        $this->assertEquals($host,'example.com');
-
+        $this->assertEquals($host, 'example.com');
     }
 
-    public function testGetPort(){
-
+    public function testGetPort()
+    {
         $urlParser = new UrlParser($this->correctUrl);
         $port = $urlParser->getPort();
-        $this->assertEquals($port,'80');
-
+        $this->assertEquals($port, '80');
     }
 
-    public function testGetPath(){
-
+    public function testGetPath()
+    {
         $urlParser = new UrlParser($this->correctUrl);
         $path = $urlParser->getPath();
-        $this->assertEquals($path,'/test');
-
+        $this->assertEquals($path, '/test');
     }
 
-    public function testToString(){
+    public function testToString()
+    {
         $urlParser = new UrlParser($this->correctUrl);
         $resUrl = (string) $urlParser;
-        $this->assertEquals($resUrl,$this->correctUrl);
+        $this->assertEquals($resUrl, $this->correctUrl);
     }
-
 }
